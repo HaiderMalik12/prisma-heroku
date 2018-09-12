b9c81ba63ae3f3cdd52a5813ce16560ceb482adc
Haider Malik <haidermalik504@gmail.com>
2018-09-12 08:57:34 +0500

    fix orderby

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,46 +1,43 @@
 function feed(parent, args, ctx, info) {
-  return ctx.db.query.posts({ where: { isPublished: true } }, info);
+	return ctx.db.query.posts({ where: { isPublished: true } }, info);
 }
 function drafts(parent, args, ctx, info) {
-  return ctx.db.query.posts({ where: { isPublished: false } }, info);
+	return ctx.db.query.posts({ where: { isPublished: false } }, info);
 }
 function post(parent, { id }, ctx, info) {
-  return ctx.db.query.post({ where: { id } }, info);
+	return ctx.db.query.post({ where: { id } }, info);
 }
 async function courseFeed(parent, { skip, first, filter, orderBy }, ctx, info) {
-  const where = filter
-    ? {
-        OR: [
-          { name_contains: filter },
-          { description_contains: filter, orderBy }
-        ]
-      }
-    : {};
-  const courses = await ctx.db.query.courses(
-    { where, skip, first, orderBy },
-    `{id}`
-  );
-  const selectionField = `{
+	const where = filter
+		? {
+				OR: [{ name_contains: filter }, { description_contains: filter }]
+		  }
+		: {};
+	const courses = await ctx.db.query.courses(
+		{ where, skip, first, orderBy },
+		`{id}`
+	);
+	const selectionField = `{
    aggregate{
      count
    }
  }`;
-  const courseConnection = await ctx.db.query.coursesConnection(
-    {},
-    selectionField
-  );
-  return {
-    courseIds: courses.map(course => course.id),
-    count: courseConnection.aggregate.count
-  };
+	const courseConnection = await ctx.db.query.coursesConnection(
+		{},
+		selectionField
+	);
+	return {
+		courseIds: courses.map(course => course.id),
+		count: courseConnection.aggregate.count
+	};
 }
 function course(parent, { id }, ctx, info) {
-  return ctx.db.query.course({ where: { id } }, info);
+	return ctx.db.query.course({ where: { id } }, info);
 }
 module.exports = {
-  feed,
-  drafts,
-  post,
-  courseFeed,
-  course
+	feed,
+	drafts,
+	post,
+	courseFeed,
+	course
 };
