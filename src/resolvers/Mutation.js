1cbba49b68121059aa049253ed52e44502f068e3
Haider Malik <haidermalik504@gmail.com>
2018-09-12 08:43:35 +0500

    add app secret

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,100 +1,106 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const { getUserId } = require('../utils');
+const { getUserId, APP_SECRET } = require('../utils');
 
 function createDraft(parent, { title, text }, ctx, info) {
-  return ctx.db.mutation.createPost(
-    {
-      data: {
-        title,
-        text
-      }
-    },
-    info
-  );
+	return ctx.db.mutation.createPost(
+		{
+			data: {
+				title,
+				text
+			}
+		},
+		info
+	);
 }
 function createCourse(parent, { name, description }, ctx, info) {
-  const userId = getUserId(ctx);
-  return ctx.db.mutation.createCourse(
-    {
-      data: {
-        name,
-        description,
-        postedBy: {
-          connect: {
-            id: userId
-          }
-        }
-      }
-    },
-    info
-  );
+	const userId = getUserId(ctx);
+	return ctx.db.mutation.createCourse(
+		{
+			data: {
+				name,
+				description,
+				postedBy: {
+					connect: {
+						id: userId
+					}
+				}
+			}
+		},
+		info
+	);
 }
 function updateCourse(parent, { id, name, description }, ctx, info) {
-  const userId = getUserId(ctx);
-  return ctx.db.mutation.updateCourse(
-    {
-      data: { name, description },
-      where: { id: id }
-    },
-    info
-  );
+	const userId = getUserId(ctx);
+	return ctx.db.mutation.updateCourse(
+		{
+			data: { name, description },
+			where: { id: id }
+		},
+		info
+	);
 }
 function deletePost(parent, { id }, ctx, info) {
-  return ctx.db.mutation.deletePost({ where: { id } }, info);
+	return ctx.db.mutation.deletePost({ where: { id } }, info);
 }
 function deleteCourse(parent, { id }, ctx, info) {
-  const userId = getUserId(ctx);
-  return ctx.db.mutation.deleteCourse({ where: { id } }, info);
+	const userId = getUserId(ctx);
+	return ctx.db.mutation.deleteCourse({ where: { id } }, info);
 }
 function publish(parent, { id }, ctx, info) {
-  return ctx.db.mutation.updatePost(
-    {
-      where: { id },
-      data: { isPublished: true }
-    },
-    info
-  );
+	return ctx.db.mutation.updatePost(
+		{
+			where: { id },
+			data: { isPublished: true }
+		},
+		info
+	);
 }
 async function signup(parent, { email, password }, ctx, info) {
-  const hash = await bcrypt.hash(password, 10);
-  const user = await ctx.db.mutation.createUser(
-    {
-      data: {
-        email,
-        password: hash
-      }
-    },
-    `{id}`
-  );
-  const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-  return {
-    token,
-    user
-  };
+	const hash = await bcrypt.hash(password, 10);
+	const user = await ctx.db.mutation.createUser(
+		{
+			data: {
+				email,
+				password: hash
+			}
+		},
+		`{id}`
+	);
+	const token = jwt.sign(
+		{ userId: user.id },
+		process.env.APP_SECRET || APP_SECRET
+	);
+	return {
+		token,
+		user
+	};
 }
 async function login(parent, { email, password }, ctx, info) {
-  const user = await ctx.db.query.user({ where: { email } }, `{id password}`);
-  if (!user) {
-    throw new Error('No such a user');
-  }
-  const matched = await bcrypt.compare(password, user.password);
-  if (!matched) {
-    throw new Error('Invalid password');
-  }
-  const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-  return {
-    token,
-    user
-  };
+	const user = await ctx.db.query.user({ where: { email } }, `{id password}`);
+	if (!user) {
+		throw new Error('No such a user');
+	}
+	const matched = await bcrypt.compare(password, user.password);
+	if (!matched) {
+		throw new Error('Invalid password');
+	}
+	const token = jwt.sign(
+		{ userId: user.id },
+		process.env.APP_SECRET || APP_SECRET
+	);
+	return {
+		token,
+		user
+	};
 }
 module.exports = {
-  createDraft,
-  createCourse,
-  updateCourse,
-  deletePost,
-  deleteCourse,
-  publish,
-  signup,
-  login
+	createDraft,
+	createCourse,
+	updateCourse,
+	deletePost,
+	deleteCourse,
+	publish,
+	signup,
+	login
 };
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
-
+const APP_SECRET = 'ADSADSADAD';
 function getUserId(ctx) {
-  const Authorization = ctx.request.get('Authorization');
-  if (Authorization) {
-    const token = Authorization.replace('Bearer ', ''); //Bearer akjshdkjashdkashdksad
-    const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    return userId;
-  }
-  throw new Error('not authenticated');
+	const Authorization = ctx.request.get('Authorization');
+	if (Authorization) {
+		const token = Authorization.replace('Bearer ', ''); //Bearer akjshdkjashdkashdksad
+		const { userId } = jwt.verify(token, process.env.APP_SECRET || APP_SECRET);
+		return userId;
+	}
+	throw new Error('not authenticated');
 }
 module.exports = {
-  getUserId
+	getUserId,
+	APP_SECRET
 };
